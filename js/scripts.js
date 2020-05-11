1b2901ba6769efcbe5861da65d6d1d70b6b602af
Pearl <pbaj0157@uni.sydney.edu.au>
2020-05-11 10:54:21 +1000

    new commit

---
+++ js/scripts.js
@@ -0,0 +1,126 @@
+(function($){
+    $.fn.extend({
+        numAnim: function(options) {
+            if ( ! this.length)
+                return false;
+
+            this.defaults = {
+                endAt: 90,
+                numClass: 'autogen-num',
+                duration: 0.5,   // seconds
+                interval: 25  // ms
+            };
+            var settings = $.extend({}, this.defaults, options);
+
+            var $num = $('<span/>', {
+                'class': settings.numClass
+            });
+
+            return this.each(function() {
+                var $this = $(this);
+
+                // Wrap each number in a tag.
+                var frag = document.createDocumentFragment(),
+                    numLen = settings.endAt.toString().length;
+                for (x = 0; x < numLen; x++) {
+                    var rand_num = Math.floor( Math.random() * 10 );
+                    frag.appendChild( $num.clone().text(rand_num)[0] )
+                }
+                $this.empty().append(frag);
+
+                var get_next_num = function(num) {
+                    ++num;
+                    if (num > 9) return 0;
+                    return num;
+                };
+
+                // Iterate each number.
+                $this.find('.' + settings.numClass).each(function() {
+                    var $num = $(this),
+                        num = parseInt( $num.text() );
+
+                    var interval = setInterval( function() {
+                        num = get_next_num(num);
+                        $num.text(num);
+                    }, settings.interval);
+
+                    setTimeout( function() {
+                        clearInterval(interval);
+                    }, settings.duration * 1000 - settings.interval);
+                });
+
+                setTimeout( function() {
+                    $this.text( settings.endAt.toString() );
+                    $("#num-" + settings.endAt).addClass("generated");
+                    generated_numbers.push(settings.endAt);
+                }, settings.duration * 1000);
+            });
+        }
+    });
+})(jQuery);
+
+var generated_numbers = [];
+
+$(function(){
+    for(i=1; i<=90; i++){
+        $("<div />").addClass("num").html(i).attr("id","num-" + i).appendTo("#grid-container");
+    }
+
+    var cw = $('.num').width();
+    $('.num').css({'height':cw+'px','line-height':cw+'px'});
+});
+
+function generateNum(){
+    if(generated_numbers.length == 90){
+        alert("Full House!");
+        return;
+    }
+    while(true){
+        var i = getRandomInt(1, 90);
+        if($.inArray(i, generated_numbers) == -1){
+            //$("#number").html(i);
+            $("#number").numAnim({
+                endAt: i
+            });
+            //$("#num-" + i).addClass("generated");
+            //generated_numbers.push(i);
+            //setTimeout("generateNum()",200);
+            break;
+        }
+    }
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// onload = function() {
+//   if ('speechSynthesis' in window) {
+//     if ('speechSynthesis' in window){
+//   var synth = speechSynthesis;
+//   var flag = false;
+
+//   /* references to the buttons */
+//   var Speak = document.querySelector('#btn-generate');
+//
+//   /* click event handlers for the buttons */
+//   Speak.addEventListener('click', onClickPlay);
+//
+//   function onClickNumber() {
+//     if(!flag){
+//         flag = true;
+//         utterance = new SpeechSynthesisUtterance(
+//               document.querySelector('number').textContent);
+//         utterance.voice = synth.getVoices()[0];
+//         utterance.onend = function(){
+//             flag = false;
+//         };
+//         synth.speak(utterance);
+//     }
+//   }
+// }
+//   }
+//   else {
+//       $('#modal1').openModal();
+//   }
+// }
